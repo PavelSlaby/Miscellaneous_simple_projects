9332099f7343762bbe4d76e3c09723908f0da387
PavelSlaby <67926620+PavelSlaby@users.noreply.github.com>
2020-10-18 19:44:06 +0200

    Add files via upload
    graphical comparison of several statistical distirbutions

---
+++ distribution_graphs.py
@@ -0,0 +1,69 @@
+'''
+graphical comparison of several statistical distirbutions
+'''
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from square_divisors import square_divisors
+
+
+# parameters
+sample = 1000 #sample size
+loc = 0
+scale = 1
+shape = 2
+a = 1
+b = 5
+n = 100
+p = 0.5
+alpha = (10, 10, 3)
+dfnum = 10
+dfden = 5
+nbins = int(sample / 10)
+df = 5
+lam = 0.9
+
+# define which distributions to display
+distributions = {
+    'Beta':         np.random.beta(a, b, sample),    
+    'Binomial':     np.random.binomial(n, p, sample),  
+    'chisquare':    np.random.chisquare(df, sample),       
+    'Dirichlet':    np.random.dirichlet(alpha, sample),
+    'Exponential':  np.random.exponential(scale, sample),
+    'F distribution': np.random.f(dfnum, dfden, sample),
+    'Gamma':        np.random.gamma(shape, scale ,sample),
+    'Geometric':    np.random.geometric(p, sample),
+    'Laplace':      np.random.laplace(loc, scale, sample),
+    'Logistic':     np.random.logistic(loc, scale, sample),
+    'Lognormal':    np.random.lognormal(loc, scale, sample),
+    'Standard Normal': np.random.standard_normal(sample),
+    'Normal':       np.random.normal(loc, scale, sample),
+    'Pareto':       np.random.pareto(a, sample),
+    'Poisson':      np.random.poisson(lam, sample),
+    'standard_t':   np.random.standard_t(df, sample),
+    'uniform':      np.random.uniform(a,b, sample),
+    'weibull':      np.random.weibull(a, sample)                               
+                }
+
+num = len(distributions)
+nrows = square_divisors(num)[0] #function to determine ideal number o rows and columns
+ncols = square_divisors(num)[1]
+
+
+# generating the graph
+plt.style.use('ggplot')
+
+fig, ax = plt.subplots(ncols = ncols, nrows = nrows, figsize = (4 * ncols, 2 * nrows))
+plt.subplots_adjust(hspace = 0.6) #the amount of space reserved between plots
+i = 0
+for row in range(nrows):
+    for col in range(ncols):
+            dist = list(distributions.items())[i]
+            sns.distplot(dist[1], bins = nbins, ax = ax[row][col])
+            ax[row][col].set_title(dist[0])
+            i += 1
+            if len(distributions) < nrows * ncols and i == num: 
+                fig.delaxes(ax[nrows -1 , ncols - 1])
+                break #if there were not a break the index would go out of range in case of even numbered list
+
