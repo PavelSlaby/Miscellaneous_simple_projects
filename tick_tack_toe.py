e128e2dda5516076684c592ec73fe9910d095a44
PavelSlaby <67926620+PavelSlaby@users.noreply.github.com>
2022-01-12 15:54:44 +0100

    Update tick_tack_toe.py

--- tick_tack_toe.py
+++ tick_tack_toe.py
@@ -4,18 +4,13 @@ Created on Sun Jan  9 17:28:48 2022
 
 ## Game of Tick Tack Toe
 
-@author: pavel
+@author: pavel slaby
 """
 
 ##libraries
-import string
 
 
-
-n = 3 #the number of columns
-d = 'C' # letter of the last row
-
-full_board = """
+simple_board = """
       
                     1     2     3
               ___|_____|_____|_____|_  
@@ -30,174 +25,105 @@ full_board = """
               ___|_____|_____|_____|_
                  |     |     |     |
 """
-
-
-letters = string.ascii_uppercase
-all_positions = []
-
-# get the list of possible positions
-for l in range(0, letters.find(d) + 1):
-    for i in range(1,  n + 1):
-        all_positions.append( str(i) + letters[l]   )
-
-
-# show the board
-
-
-intro_text = """
-            This is a game of Tick Tack Toe
-            The board is showed below.
-            When asked, input the position on the board, where you want to place your X / O
-            
-            Good luck :-) 
-"""
-
-
-clean_board = full_board    
-
-# clean the board print    
-for i in range( 0, len(all_positions)):
-    clean_board = clean_board.replace(all_positions[i] , "  ")
-
-print(intro_text)
-print(clean_board)
-
-
-## Initialization
-single_position = all_positions.copy()
-current_board = full_board
-removed = []
-moves_hist = {}
-
-# make a move
-def move(x, p, moves_hist):
-    #x is the position on the board
-    #p is the player number
-    current_board = full_board    
-    single_position = all_positions.copy()
-    
-    removed.append(x)
-    
-    for i in removed: single_position.remove(i)
-                
-    for i in range( 0, len(single_position)):
-        current_board = current_board.replace(single_position[i] , "  ")
-     
-    moves_hist[x] = p    
-     
-    for move, player in moves_hist.items():
-        if player == 1:
-            tick = " X"
-        else:
-            tick = " O"
-            
-        current_board = current_board.replace(move , tick)
-
-    print(current_board)
-
-    return moves_hist
-
-
-
-p1 = "Player 1, it is your turn:"
-p2 = "Player 2, it is your turn:"
-
-
-move = input(p1)
-# make some error handling if not inputed correctly
-
-
-
-
-x = '3B'
-p = 2
-
-moves_hist = move("3B", 2, moves_hist)
-moves_hist = move("1A", 1, moves_hist)
-moves_hist = move("1B", 2, moves_hist)
-
+print(simple_board)
 
 
 class TickTack():
      
-    def __init__(self, n = 3, d = 'C'):
+    def __init__(self, n_col = 3, n_let = 3):
         self.help = """
                     This is a game of Tick-Tack-Toe
-                    The board is showed below.
                     When asked, input the position on the board, where you want to place your X / O
                     
                     Good luck :-) 
                     """
-        self.n = n
-        self.d = d
-        self.full_board = """
-      
-                    1     2     3
-              ___|_____|_____|_____|_  
-                 |     |     |     |
-              A  | 1A  |  2A |  3A |
-              ___|_____|_____|_____|_
-                 |     |     |     |
-              B  | 1B  |  2B |  3B |
-              ___|_____|_____|_____|_
-                 |     |     |     |
-              C  | 1C  |  2C |  3C |
-              ___|_____|_____|_____|_
-                 |     |     |     |
-            """
-        
-        
+       
+        self.n_col = n_col
+        self.n_let = n_let
+        self.clean_board = str()
+        self.all_positions = []
+        self.moves_hist = {}
+        self.current_board = str()
+        self.__removed = []
+         
         import string
-        import sys 
-        
-        self.__letters = string.ascii_uppercase
-        self.__all_positions = []
+        self.letters = string.ascii_uppercase
 
         # get the list of possible positions
-        for l in range(0, self.__letters.find(self.d) + 1):
-            for i in range(1,  self.n + 1):
-                self.__all_positions.append( str(i) + self.__letters[l]   )
-
-        self.__clean_board = self.full_board    
-
-        # clean the board print    
-        for i in range( 0, len(self.__all_positions)):
-            self.__clean_board = self.__clean_board.replace(self.__all_positions[i] , "  ")
-
-
-        self.__single_position = self.__all_positions.copy()
-        self.current_board = self.full_board
-        self.__removed = []
-        self.__moves_hist = {}
+        for l in self.letters[:self.n_let]:
+            for i in range(1,  self.n_col + 1):
+                self.all_positions.append( str(i) + l   )
     
+    def prepare_board(self, clean = None):
+       
+        string = " " * 4
+
+        for i in range(1, self.n_col + 1):
+            string = string  + str(i) + " " * 4
+
+        string += '\n'
+        string += '__|' + '____|' * (self.n_col) + "_"
+
+
+        string += '\n'
+
+
+        for L in self.letters[:self.n_let]:
+            
+            string += '  |' + '    |' * (self.n_col)
+            string += '\n'
+            
+            string += L + " |"
+            
+            for i in range(1, self.n_col + 1):
+                string += " " + str(i) + L + " |"    
+            
+            string += '\n'
+            string += '__|' + '____|' * (self.n_col) + "_"
+            
+            string += '\n'
+
+        string += '  |' + '    |' * (self.n_col)
+        
+        if clean == True:
+            # clean the board print   
+            self.clean_board = string
+            
+            for i in range( 0, len(self.all_positions)):
+                self.clean_board = self.clean_board.replace(self.all_positions[i] , "  ")
+            return self.clean_board
+        else:  
+            return string
+    
+    def show_board(self):
+        print(self.prepare_board(1==1))
     
     
     def intro(self):
        print(self.help)
-       print(self.__clean_board)
-
- 
+       self.show_board()
+       
     def move(self, x, p, moves_hist = {}):
        #x is the position on the board
        #p is the player number
        
-       if x not in self.__all_positions:
-           print("Wrong input, you can only input one of the following positions: " + str(self.__all_positions))
+       if x not in self.all_positions:
+           print("Wrong input, you can only input one of the following positions: " + str(self.all_positions))
         
-       elif x in self.__moves_hist:
-           print("Wrong input, you can only input a position that was not entered before: " + str(list(set(self.__all_positions).difference(set(self.__moves_hist)))))
+       elif x in self.moves_hist:
+           print("Wrong input, you can only input a position that was not entered before: " + str(list(set(self.all_positions).difference(set(self.moves_hist)))))
                
        else:
                   
-           self.current_board = self.full_board    
-           self.__single_position = self.__all_positions.copy()
+           self.current_board = self.prepare_board(False) 
+           single_position = self.all_positions.copy()
            
            self.__removed.append(x)
            
-           for i in self.__removed: self.__single_position.remove(i)
+           for i in self.__removed: single_position.remove(i)
                        
-           for i in range( 0, len(self.__single_position)):
-               self.current_board = self.current_board.replace(self.__single_position[i] , "  ")
+           for i in range( 0, len(single_position)):
+               self.current_board = self.current_board.replace(single_position[i] , "  ")
             
            moves_hist[x] = p    
             
@@ -210,38 +136,21 @@ class TickTack():
                self.current_board = self.current_board.replace(move , tick)
     
            print(self.current_board)
-           self.__moves_hist = moves_hist
+           self.moves_hist = moves_hist
         
         
     def history(self):
         print(self.__moves_hist)
         
-        
-n = 3 #the number of columns
-d = 'C' # letter of the last row
-
+    
 a = TickTack()
-print(a.help)
-print(a.full_board)
+a.all_positions
+a.prepare_board()
+a.show_board()
 a.intro()
-a.history()
 
 a.move('1B', 1 )
 a.move('3A', 2 )
+a.move('1B', 1 )
 
-
-p = '2B'
-
-if len(p) != 2 or p[0].isalpha() or p[1].isdigit():
-    print('error')
-
-
-moves_hist
-all_positions.sort()
-
-
-
-
-a.n
-a.d
 
